4d533514db07b0cfc7e4db380798be3531a92e67
Eric Anopolsky <erpo41@gmail.com>
2020-12-04 22:42:26 -0700

    cleanup day 5

--- day5_part2.py
+++ day5_part2.py
@@ -7,26 +7,12 @@ boarding_passes = loader.string_list("input_day5")
 def convert_pass_to_row_col(bpass):
     row = 0
     col = 0
-    if bpass[0] == "B":
-        row += 64
-    if bpass[1] == "B":
-        row += 32
-    if bpass[2] == "B":
-        row += 16
-    if bpass[3] == "B":
-        row += 8
-    if bpass[4] == "B":
-        row += 4
-    if bpass[5] == "B":
-        row += 2
-    if bpass[6] == "B":
-        row += 1
-    if bpass[7] == "R":
-        col += 4
-    if bpass[8] == "R":
-        col += 2
-    if bpass[9] == "R":
-        col += 1
+    for i in range(7):
+        if bpass[i] == "B":
+            row += 2 ** (6-i)
+    for i in range(3):
+        if bpass[i+7] == "R":
+            col += 2 ** (2-i)
     return {"row": row,
             "col": col,
             "id": row * 8 + col }
@@ -38,4 +24,5 @@ max_id = max(seat_ids)
 for seat_id in range(min_id,max_id):
     if seat_id not in seat_ids:
         print(seat_id)
+        exit(0)
 
