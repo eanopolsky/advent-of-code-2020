9496e9a998ba82568b3c294da21a81ec0bc28d1c
Eric Anopolsky <erpo41@gmail.com>
2020-12-04 22:22:11 -0700

    take session cookie from file as they last for ~1 month

--- getdayinput.sh
+++ getdayinput.sh
@@ -4,11 +4,15 @@
 # to the shell's history file and will be briefly visible to other users on the
 # same machine via 'ps'.
 
-if [ $# != "2" ]
-then echo "Usage: $0 daynumber sessioncookie."
-     echo "Example: $0 5 '01234567890abcdef' # would download the input for day 5."
+# Generated 2020-12-04 and lasts for ~1 month according to the API documentation
+# on the private leaderboard page.
+SESSIONCOOKIE=$(cat my_session_cookie.txt) 
+
+if [ $# != "1" ]
+then echo "Usage: $0 daynumber "
+     echo "Example: $0 5 # would download the input for day 5."
      exit 1
 fi
 
-curl -b "session=$2" -o "input_day$1" "https://adventofcode.com/2020/day/$1/input"
+curl -b "session=$SESSIONCOOKIE" -o "input_day$1" "https://adventofcode.com/2020/day/$1/input"
 
