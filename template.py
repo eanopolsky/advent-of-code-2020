40cab538452e9af1146a1b69ad2cdfd3cb7d1155
Eric Anopolsky <erpo41@gmail.com>
2020-12-05 22:35:52 -0700

    add solution template for a head start

---
+++ template.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+import loader
+
+#inp = loader.string_list("input_day")
+#inp = loader.integer_list("input_day")
+#inp = loader.blank_line_delimited("input_day")
+#inp = loader.CharacterGrid("input_day")
+
