a07511f833288254860be7f26842513a2597d142
Eric Anopolsky <erpo41@gmail.com>
2020-12-14 23:45:30 -0700

    performance optimizations

--- day15_part2.py
+++ day15_part2.py
@@ -6,16 +6,7 @@ inp = loader.string_list("input_day15")
 
 number_list = [int(i) for i in inp[0].split(",")]
 
-number_history = {}
-
-def generate_new_number(number_history, last_said_number, last_said_number_index):
-    """
-    Returns the next number spoken.
-    """
-    if last_said_number not in number_history.keys():
-        return 0
-    else:
-        return last_said_number_index - number_history[last_said_number]
+number_history = [None for i in range(30000000)]
 
 for i in range(len(number_list)-1):
     number_history[number_list[i]] = i
@@ -24,11 +15,14 @@ last_said_number_index = len(number_list)-1
 last_said_number = number_list[-1]
 
 while True:
-    new_number = generate_new_number(number_history, last_said_number, last_said_number_index)
+    previous_last_said_number_index = number_history[last_said_number]
+    if previous_last_said_number_index == None:
+        new_number = 0
+    else:
+        new_number = last_said_number_index - previous_last_said_number_index
     number_history[last_said_number] = last_said_number_index
     last_said_number = new_number
     last_said_number_index += 1
     if last_said_number_index == 30000000-1:
         print(last_said_number)
         break
-    
